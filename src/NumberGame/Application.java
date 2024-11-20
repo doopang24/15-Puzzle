96f276802e7c8aa56d8bd43789ea2e8a893409f8
wodus4145@naver.com <wodus4145@naver.com>
2024-11-20 23:14:47 +0900

    feat: Application 클래스 생성

---
+++ src/NumberGame/Application.java
@@ -0,0 +1,16 @@
+package NumberGame;
+
+import java.util.*;
+
+public class Application {
+
+    public void start() {
+        System.out.println("재미있는 15 퍼즐!");
+    }
+
+    public static void main(String[] args) {
+        Application application = new Application();
+
+
+    }
+}
