dee2821a22c00c1f4e2c66c77dfa9c4c7f26b76e
wodus4145@naver.com <wodus4145@naver.com>
2024-11-21 10:36:12 +0900

    refactor: 시작 멘트를 출력하고 puzzle 배열을 반환하는 start() 작성

--- src/NumberGame/Application.java
+++ src/NumberGame/Application.java
@@ -4,8 +4,16 @@ import java.util.*;
 
 public class Application {
 
-    public void start() {
+    public int[][] start() {
         System.out.println("재미있는 15 퍼즐!");
+        int[][] numbers = new int[4][4];
+        int length = numbers.length;
+        for (int i = 0; i < length; i++) {
+            for (int j = 0; j < length; j++) {
+                numbers[i][j] = (i * 4) + j;    // 0~15까지 숫자 넣음
+            }
+        }
+        return shuffle(numbers);
     }
 
     public static void main(String[] args) {
