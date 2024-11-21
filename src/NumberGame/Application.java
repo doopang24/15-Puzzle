0bdd7e518cfb7b091b199f299aa710e27d6ffbe3
wodus4145@naver.com <wodus4145@naver.com>
2024-11-21 12:15:29 +0900

    feat: 2차원 puzzle이 순서에 맞게 정렬되었는지 확인하는 isOrdered() 메서드 작성

--- src/NumberGame/Application.java
+++ src/NumberGame/Application.java
@@ -12,9 +12,9 @@ public class Application {
 
         int[][] puzzle = application.start();
         int turn = 1;
-        boolean isSorted = false;
+        boolean isSolved = false;
 
-        while (!isSorted) {
+        while (!isSolved) {
             application.printStatus(puzzle, turn);
         }
 
@@ -62,5 +62,28 @@ public class Application {
                 System.out.println();
             }
         }
+    }
+
+    public boolean isOrdered(int[][] puzzle) {
+        int verticalLength = 3;
+        int horizonLength = 4;
+        int count = 0;
+        int totalGoal = 16;
+        for (int i = 0; i < verticalLength; i++) {
+            for (int j = 0; j < horizonLength; j++) {
+                if (puzzle[i][j] == (i * 4) + j + 1) {
+                    count++;
+                }
+            }
+        }
+        for(int j=0; j<horizonLength-1; j++) {
+            if(puzzle[3][j] == 12 + j + 1) {
+                count++;
+            }
+        }
+        if(puzzle[verticalLength+1][horizonLength] == 0) {
+            count++;
+        }
+        return count == totalGoal;
     }
 }
