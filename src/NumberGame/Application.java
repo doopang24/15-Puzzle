88b1a37cbecca0bf3e94b4e35c2f46b2718d57ea
wodus4145@naver.com <wodus4145@naver.com>
2024-11-21 10:37:25 +0900

    feat: 2차원 puzzle 배열을 섞는 shuffle() 메서드 작성

--- src/NumberGame/Application.java
+++ src/NumberGame/Application.java
@@ -1,8 +1,26 @@
 package NumberGame;
 
 import java.util.*;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class Application {
+
+    ThreadLocalRandom random = ThreadLocalRandom.current();
+
+    public int[][] shuffle(int[][] numbers) {
+        int length = numbers.length;
+        int bound = 4;
+        for (int i = 0; i < length; i++) {
+            for (int j = 0; j < length; j++) {
+                int verticalTarget = random.nextInt(bound); // i
+                int horizonTarget = random.nextInt(bound);  // j
+                int tmp = numbers[i][j];
+                numbers[i][j] = numbers[verticalTarget][horizonTarget];
+                numbers[verticalTarget][horizonTarget] = tmp;
+            }
+        }
+        return numbers;
+    }
 
     public int[][] start() {
         System.out.println("재미있는 15 퍼즐!");
@@ -19,6 +37,8 @@ public class Application {
     public static void main(String[] args) {
         Application application = new Application();
 
+        int[][] puzzle = application.start();
+        int turn = 1;
 
     }
 }
